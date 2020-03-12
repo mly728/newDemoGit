477c7f8b1d8e7976c8f745d4a7c3cfb1eae705f8
mly728 <941824026@qq.com>
2020-03-12 16:08:17 +0800

    NodeJS

---
+++ home.js
@@ -0,0 +1,11 @@
+var slide3 = new auiSlide({
+    container:document.getElementById("aui-slide3"),
+    // "width":300,
+    "height":240,
+    "speed":500,
+    "autoPlay": 3000, //自动播放
+    "loop":true,
+    "pageShow":true,
+    "pageStyle":'line',
+    'dotPosition':'center'
+})
